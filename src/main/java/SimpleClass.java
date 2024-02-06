660e159a4824f68b92b3b933134e46a70e22bbee
Esteban Dujardin <este22pro@gmail.com>
2024-02-06 12:00:21 +0100

    Renommage Final

--- src/main/java/SimpleClass.java
+++ src/main/java/SimpleClass.java
@@ -1,8 +1,8 @@
 public class SimpleClass {
     public static void main(String[] args) {
-        new SimpleClass().someAdditionMethod(1,2);
+        new SimpleClass().add(1,2);
     }
-    public int someAdditionMethod(int a, int b) {
+    public int add(int a, int b) {
         return a+b;
     }
 }
