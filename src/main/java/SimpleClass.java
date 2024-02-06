c74bbbaa282bdc7e824e808a34cbd4ef9f6822b3
Esteban Dujardin <este22pro@gmail.com>
2024-02-06 11:58:23 +0100

    Renommage initial

---
+++ src/main/java/SimpleClass.java
@@ -0,0 +1,8 @@
+public class SimpleClass {
+    public static void main(String[] args) {
+        new SimpleClass().someAdditionMethod(1,2);
+    }
+    public int someAdditionMethod(int a, int b) {
+        return a+b;
+    }
+}
