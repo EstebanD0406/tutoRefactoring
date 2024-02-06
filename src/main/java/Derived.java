0baceb645a9997adfe88578d9ea1c57d70e96c4c
Esteban Dujardin <este22pro@gmail.com>
2024-02-06 14:34:47 +0100

    Tirer vers le haut

---
+++ src/main/java/Derived.java
@@ -0,0 +1,7 @@
+public class Derived extends Base {
+    public static void main(String[] args) {
+        Derived subject = new Derived();
+        System.out.println("Doubling 21. Result: " + subject.doubleValue(21));
+    }
+
+}
