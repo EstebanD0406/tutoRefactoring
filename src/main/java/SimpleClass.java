24cc47fb3c30ae9538843a22fd2836ae36d0e83b
Esteban Dujardin <este22pro@gmail.com>
2024-02-06 12:09:17 +0100

    Renommage avancée initial

--- src/main/java/SimpleClass.java
+++ src/main/java/SimpleClass.java
@@ -1,8 +1,23 @@
+/**
+ * SimpleClass
+ *
+ * @author Esteban Dujardin (este22pro@gmail.com)
+ */
 public class SimpleClass {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        new SimpleClass().add(1,2);
+        new SimpleClass().add(1, 2);
     }
+
+    /**
+     * Adds a and b
+     *
+     * @param a the first number
+     * @param b the second number
+     */
     public int add(int a, int b) {
-        return a+b;
+        return a + b;
     }
 }
